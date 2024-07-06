f2b3a8d0f304048283ead78650dcc3100454a0d5
Wiktor <lolxdyoloelo@gmail.com>
2024-07-06 21:17:06 +0200

    base template

---
+++ src/pages-app/timelines-app.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./timelines-app.css";
+
+function TimelinesApp() {
+  return (
+    <>
+      <div className="main">Tu bedzie aplikacja</div>
+    </>
+  );
+}
+
+export default App;
