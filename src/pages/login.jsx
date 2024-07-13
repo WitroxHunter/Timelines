c8933bac9c5e96facb6dd22f83f3420aaedf3671
Wiktor <lolxdyoloelo@gmail.com>
2024-07-13 14:29:48 +0200

    dodano opcje resetowania hasla

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,6 +3,7 @@ import { Navigate, Link } from "react-router-dom";
 import {
   doSignInWithEmailAndPassword,
   doSignInWithGoogle,
+  doPasswordReset,
 } from "../firebase/auth";
 import { useAuth } from "../contexts/authContext";
 import "./login.css";
@@ -16,6 +17,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [isSigningIn, setIsSigningIn] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [resetMessage, setResetMessage] = useState("");
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -43,6 +45,20 @@ export default function Login() {
     }
   };
 
+  const onPasswordReset = async (e) => {
+    e.preventDefault();
+    if (email) {
+      try {
+        await doPasswordReset(email);
+        setResetMessage("Password reset email sent!");
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    } else {
+      setErrorMessage("Please enter your email address.");
+    }
+  };
+
   if (userLoggedIn) {
     return <Navigate to="/Timelines/app" />;
   }
@@ -50,9 +66,10 @@ export default function Login() {
   return (
     <>
       <div className="main">
-        <img src={Logo} className="logo" />
+        <img src={Logo} className="logo" alt="Logo" />
         <h2>Welcome back</h2>
         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        {resetMessage && <p style={{ color: "green" }}>{resetMessage}</p>}
         <form onSubmit={onSubmit}>
           <div className="input-box">
             <label className="label">Email</label>
@@ -77,6 +94,14 @@ export default function Login() {
           Don't have an account?
           <span className="blue-font">
             <Link to="/Timelines/sign-up"> Sign Up</Link>
+          </span>
+          <br />
+          <span
+            className="blue-font"
+            onClick={onPasswordReset}
+            style={{ cursor: "pointer" }}
+          >
+            I don't remember my password
           </span>
           <br />
           <button
--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,36 +1,87 @@
+// src/components/SignUp.js
+import React, { useState } from "react";
 import "./sign-up.css";
 import Logo from "../assets/images/Logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { doCreateUserWithEmailAndPassword } from "../firebase/auth";
 
 export default function SignUp() {
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== repeatPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    try {
+      await doCreateUserWithEmailAndPassword(email, password);
+      // Navigate to a different page after successful registration
+      navigate("/Timelines");
+    } catch (error) {
+      console.error("Error creating user:", error);
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       <div className="main">
-        <img src={Logo} className="logo" />
+        <img src={Logo} className="logo" alt="Logo" />
         <h2>Create account</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-box">
             <label className="label">Nickname</label>
-            <input className="input" type="text" />
+            <input
+              className="input"
+              type="text"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
           </div>
           <div className="input-box">
             <label className="label">Email</label>
-            <input className="input" type="email" />
+            <input
+              className="input"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="input-box">
             <label className="label">Password</label>
-            <input className="input" type="password" />
+            <input
+              className="input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input-box">
             <label className="label">Repeat password</label>
-            <input className="input" type="password" />
+            <input
+              className="input"
+              type="password"
+              value={repeatPassword}
+              onChange={(e) => setRepeatPassword(e.target.value)}
+            />
           </div>
+          {error && <p className="error">{error}</p>}
           Already have an account?
           <Link to="/Timelines/login">
             <span className="blue-font"> Log in</span>
           </Link>
           <br />
-          <input type="submit" className="submit-button" value={"Submit"} />
+          <input type="submit" className="submit-button" value="Submit" />
         </form>
       </div>
     </>
