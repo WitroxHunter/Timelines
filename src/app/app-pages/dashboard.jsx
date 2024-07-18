10af6a7dc487d6efd63ac4dde6c5bb63520f620e
Wiktor <lolxdyoloelo@gmail.com>
2024-07-18 10:45:54 +0200

    badziewia i pierdoły

--- src/app/app-pages/dashboard.jsx
+++ src/app/app-pages/dashboard.jsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import "./dashboard.css";
+import Header from "../app-components/header";
 function Dashboard() {
   return (
     <>
       <div className="main">
+        <Header></Header>
         Wybierz timeline
         <div className="timelines-box">
           <button className="add-timeline-button">Dodaj timeline</button>
-          <Link to={"timeline"}>
-            <button>Nawiguj do timeline testowego</button>
-          </Link>
+          <div className="timeline-button-box">
+            <Link to={"timeline"}>
+              <button className="timeline-button"></button>
+            </Link>
+            Timeline 1
+          </div>
         </div>
       </div>
     </>
