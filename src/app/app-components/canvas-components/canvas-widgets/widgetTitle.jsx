1ee7ba245a75b6ec060eda8028feb31de4564090
KubixekTV <73798453+KubixekTv@users.noreply.github.com>
2024-09-24 18:46:02 +0200

    Fixed input for timeline name change with duct tape

--- src/app/app-components/canvas-components/canvas-widgets/widgetTitle.jsx
+++ src/app/app-components/canvas-components/canvas-widgets/widgetTitle.jsx
@@ -2,7 +2,13 @@ import React, { useState, useRef } from "react";
 import { changeTimelineName } from "../canvas-firestore-actions/firebaseUtils";
 import editIcon from "../../../../assets/icons/edit.svg";
 
-const TimelineTitleEditor = ({ timelineTitle, setTimelineTitle, timelineData, currentUser, timelineId }) => {
+const TimelineTitleEditor = ({
+  timelineTitle,
+  setTimelineTitle,
+  timelineData,
+  currentUser,
+  timelineId,
+}) => {
   const [isEditing, setIsEditing] = useState(false);
   const titleInputRef = useRef(null);
 
@@ -13,19 +19,31 @@ const TimelineTitleEditor = ({ timelineTitle, setTimelineTitle, timelineData, cu
     }
   };
 
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    // Ograniczamy liczbę znaków do 20
+    if (value.length <= 30) {
+      setTimelineTitle(value);
+    }
+  };
+
   return (
-    <div className="opened-timeline-file-name" onClick={() => setIsEditing(true)}>
+    <div
+      className="opened-timeline-file-name"
+      onClick={() => setIsEditing(true)}
+    >
       {isEditing ? (
         <input
           ref={titleInputRef}
-          className="edit-timeline-title-input"
+          className="edit-timeline-title-input-editing"
           value={timelineTitle}
-          onChange={(e) => setTimelineTitle(e.target.value)}
+          onChange={handleInputChange}
           onBlur={handleSaveTitle}
           autoFocus
+          style={{ width: `${timelineTitle.length}ch` }} // Dynamiczna szerokość inputu
         />
       ) : (
-        <div>{timelineTitle}</div>
+        <div className="edit-timeline-title-input">{timelineTitle}</div>
       )}
       <img src={editIcon} alt="Edit Icon" />
     </div>
