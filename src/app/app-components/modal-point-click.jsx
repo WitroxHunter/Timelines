daab51a1e3e28b99239adf5998b3d9f6419851e8
Wiktor <80409661+WitroxHunter@users.noreply.github.com>
2024-11-10 23:33:01 +0100

    point modal updated

--- src/app/app-components/modal-point-click.jsx
+++ src/app/app-components/modal-point-click.jsx
@@ -12,9 +12,9 @@ export default function ModalPoint({
 }) {
   if (!isOpen || !point) return null;
 
-  const formattedDate = `${point.date.getDate()}-${
+  const formattedDate = `${point.date.getDate()}.${
     point.date.getMonth() + 1
-  }-${point.date.getFullYear()}`;
+  }.${point.date.getFullYear()}`;
 
   const handleDelete = async () => {
     if (window.confirm("Are you sure you want to delete this point?")) {
@@ -24,17 +24,25 @@ export default function ModalPoint({
   };
 
   return (
-    <div className="modal-overlay" onClick={toggleModal}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="modal-overlay-right" onClick={toggleModal}>
+      <div className="modal-content-right" onClick={(e) => e.stopPropagation()}>
         <h2>{point.label}</h2>
-        <p>{point.description}</p>
+        
+        <div className="date-color-line">
         <p>{formattedDate}</p>
+          <div
+            className="color-line"
+            style={{ backgroundColor: point.color }}
+          ></div>
+        </div>
+        <p>{point.description}</p>
+        
         {children}
-        <button className="modal-button" onClick={handleDelete}>
+        <button className="modal-button-right" onClick={handleDelete}>
           Delete point
         </button>
-        <button className="modal-button" onClick={toggleModal}>
-          Leave
+        <button className="modal-button-right" onClick={toggleModal}>
+          Close
         </button>
       </div>
     </div>
